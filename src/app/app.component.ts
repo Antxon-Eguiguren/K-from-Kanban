21d7ad79670073a2bc3718d4a1435252f426428e
Antxon <antxonegi@gmail.com>
2021-10-01 14:24:39 +0200

    Drag & Drop implemented, pending styles when tile is empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 import { Task } from './interfaces/task.model';
 import { TaskService } from './services/task.service';
@@ -24,18 +24,10 @@ export class AppComponent implements OnInit, OnDestroy {
   
   ngOnInit(): void {
     this.subscription = this.taskService.getTasks().subscribe(tasks => {
-      this.newTasks = [];
-      this.inProgressTasks = [];
-      this.finishedTasks = [];
-      tasks.forEach(task => {
-        if (task.status === 'New') {
-          this.newTasks.push(task);
-        } else if (task.status === 'In Progress') {
-          this.inProgressTasks.push(task);
-        } else if (task.status === 'Finished') {
-          this.finishedTasks.push(task);
-        }
-      });
+      this.tasks = tasks;
+      this.newTasks = tasks.filter(task => task.status === 'New');
+      this.inProgressTasks = tasks.filter(task => task.status === 'In Progress');
+      this.finishedTasks = tasks.filter(task => task.status === 'Finished');
       this.isLoading = false;
     });
   }
@@ -44,22 +36,66 @@ export class AppComponent implements OnInit, OnDestroy {
     console.log('Clicked on new task...');
   }
 
-  dropped(event: any) {
+  dropped(event: CdkDragDrop<string[]>): void {
     if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    }  
-   //this.boardService.sortBoards(this.boards);
+      if (event.container.id === 'new') {
+        moveItemInArray(this.newTasks, event.previousIndex, event.currentIndex);
+      } else if (event.container.id === 'inProgress') {
+        moveItemInArray(this.inProgressTasks, event.previousIndex, event.currentIndex);
+      } else if (event.container.id === 'finished') {
+        moveItemInArray(this.finishedTasks, event.previousIndex, event.currentIndex);
+      }
+    } else if (event.previousContainer !== event.container) {
+      if ((event.previousContainer.id === 'new') && (event.container.id === 'inProgress') || 
+          (event.previousContainer.id === 'new') && (event.container.id === 'inProgress-empty')) {
+        transferArrayItem(
+          this.newTasks,
+          this.inProgressTasks,
+          event.previousIndex,
+          event.currentIndex
+        );
+      } else if ((event.previousContainer.id === 'inProgress') && (event.container.id === 'new') ||
+                 (event.previousContainer.id === 'inProgress') && (event.container.id === 'new-empty')) {
+        transferArrayItem(
+          this.inProgressTasks,
+          this.newTasks,
+          event.previousIndex,
+          event.currentIndex
+        );
+      } else if ((event.previousContainer.id === 'inProgress') && (event.container.id === 'finished') ||
+                 (event.previousContainer.id === 'inProgress') && (event.container.id === 'finished-empty')) {
+        transferArrayItem(
+          this.inProgressTasks,
+          this.finishedTasks,
+          event.previousIndex,
+          event.currentIndex
+        );
+      } else if ((event.previousContainer.id === 'finished') && (event.container.id === 'inProgress') ||
+                 (event.previousContainer.id === 'finished') && (event.container.id === 'inProgress-empty')) {
+        transferArrayItem(
+          this.finishedTasks,
+          this.inProgressTasks,
+          event.previousIndex,
+          event.currentIndex
+        );
+      } else if ((event.previousContainer.id === 'new') && (event.container.id === 'finished') ||
+                 (event.previousContainer.id === 'new') && (event.container.id === 'finished-empty')) {
+        transferArrayItem(
+          this.newTasks,
+          this.finishedTasks,
+          event.previousIndex,
+          event.currentIndex
+        );
+      } else if ((event.previousContainer.id === 'finished') && (event.container.id === 'new') ||
+                 (event.previousContainer.id === 'finished') && (event.container.id === 'new-empty')) {
+        transferArrayItem(
+          this.finishedTasks,
+          this.newTasks,
+          event.previousIndex,
+          event.currentIndex
+        );
+      }
+    }
   }
 
   ngOnDestroy(): void {
